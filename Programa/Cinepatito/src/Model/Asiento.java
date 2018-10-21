9338f05f319970e1ffbc195cb19b963b557dcd99
Edwin <erofaba@gmail.com>
2018-10-21 16:51:05 -0500

    Avances en la interfaz gráfica de los asientos de la sala

---
+++ Programa/Cinepatito/src/Model/Asiento.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author erofa
+ */
+class Asiento {
+
+    private int id;
+    private boolean disponible;
+
+    public Asiento(int id, boolean disponible) {
+        this.id = id;
+        this.disponible = disponible;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+
+    public void cancelar() {
+
+    }
+
+}
---
+++ Programa/Cinepatito/src/Model/Funcion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author erofa
+ */
+public class Funcion {
+
+    private int id;
+    private int hora;
+    private int numSala;
+    private Asiento[][] asientos;
+    private boolean abierta;
+
+}
